b2b45b7b4fa9be2200d829fa326403b7cf48d2fc
vtitoff <v.titoff2010@gmail.com>
2022-05-09 17:57:53 +0100

    add selection_sort

--- Python/algorithms/selection_sort.py
+++ Python/algorithms/selection_sort.py
@@ -1,10 +1,10 @@
 massive = [5, 8, 3, 1, 2, 4, 6, 7, 9, 10, 8]
 sorted_massive = []
-length = len(massive)
-for i in range(0, length):
-    while len(massive) >= 1:
-        minimal = min(massive)
-        minimal_index = massive.index(minimal)
-        if massive[i]!=minimal:
-            massive[0]
+while len(massive):
+    length = len(massive)
+    minim = min(massive)
+    min_index = massive.index(minim)
+    massive[min_index], massive[length - 1] = massive[length - 1], massive[min_index]
+    sorted_massive.append(massive.pop())
 
+print(sorted_massive)
